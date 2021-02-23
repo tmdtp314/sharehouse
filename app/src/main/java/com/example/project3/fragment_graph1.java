759c4deb53168043ff6657fa1a419cbcb9de111a
tmdtp314 <tmdtp3143@gmail.com>
2021-02-23 20:40:43 +0900

    ok

---
+++ app/src/main/java/com/example/project3/fragment_graph1.java
@@ -0,0 +1,19 @@
+package com.example.project3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_graph1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View fragment = inflater.inflate(R.layout.fragment_graph1, container, false);
+        return fragment;
+    }
+}
